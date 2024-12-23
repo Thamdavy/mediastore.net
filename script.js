1e2aa2f8638e94dbe29116fcdd65597f58046bbc
Thamdavy <165388252+Thamdavy@users.noreply.github.com>
2024-12-23 16:00:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example for search button interaction
+document.querySelector('.search-button').addEventListener('click', () => {
+    const query = document.querySelector('.search-input').value;
+    alert(`You searched for: ${query}`);
+  });
+  
